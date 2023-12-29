22279d12917d6acbacb88b3d97fb42b34b11170b
vivek541 <111304197+vivek541@users.noreply.github.com>
2023-12-29 12:29:11 +0530

    Add files via upload

---
+++ stackApplications-InfixToPostfix(combinedOps)_usingArrayStack.c
@@ -0,0 +1,179 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+#include<stdlib.h>
+#define MAX 100
+/*WAP to convert a given infix expression to its equivalent postfix form and print it
+  also, print the value obtained after evaluating the postfix expression so obtained*/
+
+//global stuff...
+char infix[MAX], postfix[MAX];
+char stack[MAX];
+int top = -1;
+
+bool isEmpty(){
+
+    if(top == -1) return true;
+    else          return false;
+}
+
+bool isFull(){
+
+    if(top == MAX-1) return true;
+    else             return false;
+}
+
+void push(char data){
+
+    if(isFull()){
+        printf("stack overflow!!\n");
+        exit(1);
+    }
+    else{
+        top++;
+        stack[top] = data;
+    }
+}
+
+char pop(){
+
+    if(isEmpty()){
+        printf("stack underflow!!\n");
+        exit(2);
+    }
+    else{
+
+        char retVal = stack[top];
+        top--;
+
+        return retVal;
+    }
+}
+
+int precedence(char symbol){  //AWESOME precedence checking function:)
+
+    switch(symbol){
+
+    case '^' :
+        return 3;
+    case '*' :
+    case '/' :
+        return 2;
+    case '+' :
+    case '-' :
+        return 1;
+    default  :
+        return 0;
+    }
+}
+
+void infixToPostfix(){
+
+    int i = 0, k = 0;
+    char sym;
+
+    //1. main.....................................................................................................
+
+    while(infix[i] != '\0'){
+
+        sym = infix[i];
+
+        if(sym == ' ' || sym == '\t'){i++; continue;}  /*to bypass spaces and tabs*/
+
+        if(sym >= '0' && sym <= '9' ||
+           sym >= 'a' && sym <= 'z' ||
+           sym >= 'A' && sym <= 'Z'){                  /**CASE1 -> when sym is an operand*/
+            postfix[k++] = sym;
+        }
+        else if(sym == '('){                           /**CASE2 -> when sym is '(' */
+            push(sym);
+        }
+        else if(sym == ')'){                           /**CASE3 -> when sym is ')' */
+            while(!isEmpty() && stack[top] != '('){
+                postfix[k++] = pop();
+            }
+            pop(); /*to discard '('*/
+        }
+        else{                                          /**CASE4 -> when sym is an operator*/
+
+//            int topBckp = top;    /*note that we can't use org top for iterating the stack*/
+//
+//            while(topBckp != -1 && stack[topBckp] != '('){
+//                if(precedence(sym) <= precedence(stack[topBckp])){  /*precedence check as per the algorithm*/
+//                    postfix[k++] = pop();
+//                    topBckp = top;          /*VIMP**/
+//                }
+//                else topBckp--;             /*VIMP**/
+//            }
+//            push(sym);
+
+            while (!isEmpty() && stack[top] != '(' && precedence(sym) <= precedence(stack[top])) {
+                postfix[k++] = pop();
+            }
+            push(sym);
+
+        }
+
+        i++;
+    }
+
+    //2. casualty chek...........................................................................................
+
+    while(top > -1) postfix[k++] = pop();
+
+    //3. the null ritual...........................................................................................
+
+    postfix[k++] = '\0';
+}
+
+void evaluatePostfix(){
+
+    int i = 0;
+
+    while(postfix[i] != '\0'){
+
+        if(postfix[i] >= '0' && postfix[i] <= '9'){
+            push(postfix[i]-'0');                   /**1 step solution for all ascii anomalies**/
+        }
+        else{
+            int rightOp, leftOp;
+            rightOp = pop();
+            leftOp = pop();
+
+            if     (postfix[i] == '+')  push(leftOp + rightOp);
+            else if(postfix[i] == '-')  push(leftOp - rightOp);
+            else if(postfix[i] == '*')  push(leftOp * rightOp);
+            else if(postfix[i] == '/')  push(leftOp / rightOp);
+            else                                                /*when operator is '^' */
+                push(pow(leftOp, rightOp));
+        }
+
+        i++;
+    }
+}
+
+int main(){
+
+    printf("enter the infix expression   : ");
+    gets(infix);
+
+    infixToPostfix();
+    printf("\npostfix form               : ");
+    puts(postfix);
+/*
+    evaluatePostfix();
+    printf("\nvalue of the above postfix expression        : %d", stack[top]);
+    printf("\n------------------------------------------------------------------------\n");
+*/
+    return 0;
+}
+
+
+
+
+/*
+7+5*3/5^1+(3-2)
+7+(9-5)*2
+3+5*(5/5)-2^2
+*/
+
